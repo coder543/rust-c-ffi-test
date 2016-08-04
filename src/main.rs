39cf33dbf7ff2f62addd73a10d847bdc250863fe
Josh Leverette <coder543@gmail.com>
2016-08-04 10:50:00 -0500

    Revert "removed unnecessary null" since the null is necessary to avoid UB.
    This reverts commit 972cabb2f9edf92ffe0aded5861c281e614234b7.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate libc;
 use libc::c_uchar;
+use std::ptr;
 use std::ffi::CString;
 
 #[link(name="test")]
@@ -22,6 +23,8 @@ fn main() {
         //create a pointer to that CString's raw data and store it in c_str_ptrs
         c_str_ptrs.push(c_strs[c_strs.len() - 1].as_ptr() as *const c_uchar);
     }
+
+    c_str_ptrs.push(ptr::null()); //add a null terminator to the array of pointers
 
     unsafe { print_stuff(c_str_ptrs.as_ptr()); } //call print_stuff
 
